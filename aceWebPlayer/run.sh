0ec3a1e02fe8182927d7a022f86c054d3afa31c1
asopaipa <eslounicoquese@gmail.com>
2025-03-01 13:04:26 +0100

    Update run.sh

--- aceWebPlayer/run.sh
+++ aceWebPlayer/run.sh
@@ -1,43 +1,30 @@
 #!/bin/bash
 
 if [ -f "$CONFIG_FILE" ]; then
-    ENABLE_TOR=$(jq -r '.ENABLE_TOR // "false"' "$CONFIG_FILE")
-    UI_PASSWORD=$(jq -r '.UI_PASSWORD // ""' "$CONFIG_FILE")
+    UI_USERNAME=$(jq -r '.UI_USERNAME // "user"' "$CONFIG_FILE")
+    UI_PASSWORD=$(jq -r '.UI_PASSWORD // "user"' "$CONFIG_FILE")
+    ACESTREAM_PORT=$(jq -r '.ACESTREAM_PORT // "6878"' "$CONFIG_FILE")
 else
     echo "No se encontró $CONFIG_FILE, usando valores por defecto."
-    ENABLE_TOR="false"
-    UI_PASSWORD=""
+    UI_USERNAME="user"
+    UI_PASSWORD="user"
+    ACESTREAM_PORT="6878"
 fi
 
-
-# Pedir el puerto al usuario
-read -p "¿En qué puerto quieres que se publique la web? (5001) " PORT
-
-read -p "¿En qué puerto quieres que se publique el Acestream? (6878) " PORTACE
-
-
-
-# Preguntar si se quiere permitir el acceso remoto
-read -p "Si quieres proteger la web con usuario y contraseña, introduce el usuario: " USUARIO
-
-if [ -n "$USUARIO" ]; then
-    read -p "Introduce la contraseña: " CONTRASENYA
-fi
 
 # Exportar la variable de entorno para el puerto
-export PORT=$PORT
-export PORTACE=$PORTACE
+export PORTACE=$ACESTREAM_PORT
 
 
-if [ -n "$PORTACE" ]; then
-    sed -i "s/6878/$PORTACE/g" ./static/js/main.js
-    sed -i "s/6878/$PORTACE/g" ./getLinks.py
+if [ -n "$ACESTREAM_PORT" ]; then
+    sed -i "s/6878/$ACESTREAM_PORT/g" ./app/static/js/main.js
+    sed -i "s/6878/$ACESTREAM_PORT/g" ./app/getLinks.py
 fi
 
-if [ -n "$USUARIO" ]; then
-    sed -i "s/USERNAME = \"\"/USERNAME = \"$USUARIO\"/g" app.py
-    sed -i "s/PASSWORD = \"\"/PASSWORD = \"$CONTRASENYA\"/g" app.py
+if [ -n "$UI_USERNAME" ]; then
+    sed -i "s/USERNAME = \"\"/USERNAME = \"$UI_USERNAME\"/g" ./app/app.py
+    sed -i "s/PASSWORD = \"\"/PASSWORD = \"$UI_PASSWORD\"/g" ./app/app.py
 fi
 
 
-docker build -t acestream-player .
+exec python /app/app.py
