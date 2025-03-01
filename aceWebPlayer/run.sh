c353e412578d0234bc0293cd5283934833be9439
asopaipa <eslounicoquese@gmail.com>
2025-03-01 13:16:37 +0100

    Update run.sh

--- aceWebPlayer/run.sh
+++ aceWebPlayer/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CONFIG_FILE="/data/options.json"
+
 if [ -f "$CONFIG_FILE" ]; then
     UI_USERNAME=$(jq -r '.UI_USERNAME // "user"' "$CONFIG_FILE")
     UI_PASSWORD=$(jq -r '.UI_PASSWORD // "user"' "$CONFIG_FILE")
@@ -17,13 +19,13 @@ export PORTACE=$ACESTREAM_PORT
 
 
 if [ -n "$ACESTREAM_PORT" ]; then
-    sed -i "s/6878/$ACESTREAM_PORT/g" ./app/static/js/main.js
-    sed -i "s/6878/$ACESTREAM_PORT/g" ./app/getLinks.py
+    sed -i "s/6878/$ACESTREAM_PORT/g" /app/static/js/main.js
+    sed -i "s/6878/$ACESTREAM_PORT/g" /app/getLinks.py
 fi
 
 if [ -n "$UI_USERNAME" ]; then
-    sed -i "s/USERNAME = \"\"/USERNAME = \"$UI_USERNAME\"/g" ./app/app.py
-    sed -i "s/PASSWORD = \"\"/PASSWORD = \"$UI_PASSWORD\"/g" ./app/app.py
+    sed -i "s/USERNAME = \"\"/USERNAME = \"$UI_USERNAME\"/g" /app/app.py
+    sed -i "s/PASSWORD = \"\"/PASSWORD = \"$UI_PASSWORD\"/g" /app/app.py
 fi
 
 
