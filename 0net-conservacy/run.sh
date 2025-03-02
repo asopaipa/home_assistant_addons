70279706b307388349a9f83b78e9cee848bd479c
asopaipa <eslounicoquese@gmail.com>
2025-03-02 20:27:55 +0100

    Update run.sh

--- 0net-conservacy/run.sh
+++ 0net-conservacy/run.sh
@@ -5,15 +5,23 @@ CONFIG_FILE="/data/options.json"
 if [ -f "$CONFIG_FILE" ]; then
     ENABLE_TOR=$(jq -r '.ENABLE_TOR // "false"' "$CONFIG_FILE")
     UI_PASSWORD=$(jq -r '.UI_PASSWORD // ""' "$CONFIG_FILE")
+    TOR_CONTROL_PORT=$(jq -r '.TOR_CONTROL_PORT // "9151"' "$CONFIG_FILE")
+    TOR_SOCKS_PORT=$(jq -r '.TOR_SOCKS_PORT // "9150"' "$CONFIG_FILE")
+    PORT_FILESERVER=$(jq -r '.ports["26552/tcp"] // 26552' "$CONFIG_FILE")
 else
     echo "No se encontró $CONFIG_FILE, usando valores por defecto."
     ENABLE_TOR="false"
     UI_PASSWORD=""
+    TOR_CONTROL_PORT="9151"
+    TOR_SOCKS_PORT="9150"
+    PORT_FILESERVER="26552"
 fi
 
 export ENABLE_TOR
 export UI_PASSWORD
+export TOR_CONTROL_PORT
+export TOR_SOCKS_PORT
 
-exec python3 /home/service-0net/zeronet.py --ui_ip "*" --fileserver_port 26552 \
-    --tor $TOR_ENABLED --tor_controller tor:$TOR_CONTROL_PORT \
-    --tor_proxy tor:$TOR_SOCKS_PORT --tor_password $TOR_CONTROL_PASSWD
+exec python3 /home/service-0net/zeronet.py --ui_ip "*" --fileserver_port $PORT_FILESERVER \
+    --tor $ENABLE_TOR --tor_controller tor:$TOR_CONTROL_PORT \
+    --tor_proxy tor:$TOR_SOCKS_PORT --tor_password $TOR_CONTROL_PASSWD --ui_password $UI_PASSWORD
