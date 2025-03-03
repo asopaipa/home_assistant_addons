7f0cfe9a2e88ccd7fce86be25b1b8feb032b887a
asopaipa <eslounicoquese@gmail.com>
2025-03-03 14:10:36 +0100

    Update run.sh

--- 0net-conservacy/run.sh
+++ 0net-conservacy/run.sh
@@ -4,7 +4,6 @@ CONFIG_FILE="/data/options.json"
 
 if [ -f "$CONFIG_FILE" ]; then
     ENABLE_TOR=$(jq -r '.ENABLE_TOR // "false"' "$CONFIG_FILE")
-    UI_PASSWORD=$(jq -r '.UI_PASSWORD // ""' "$CONFIG_FILE")
     TOR_CONTROL_PORT=$(jq -r '.TOR_CONTROL_PORT // "9151"' "$CONFIG_FILE")
     TOR_SOCKS_PORT=$(jq -r '.TOR_SOCKS_PORT // "9150"' "$CONFIG_FILE")
     PORT_FILESERVER=$(jq -r '.ports["26552/tcp"] // 26552' "$CONFIG_FILE")
@@ -14,7 +13,6 @@ if [ -f "$CONFIG_FILE" ]; then
 else
     echo "No se encontró $CONFIG_FILE, usando valores por defecto."
     ENABLE_TOR="false"
-    UI_PASSWORD=""
     TOR_CONTROL_PORT="9151"
     TOR_SOCKS_PORT="9150"
     PORT_FILESERVER="26552"
@@ -23,14 +21,12 @@ else
 fi
 
 export ENABLE_TOR
-export UI_PASSWORD
 export TOR_CONTROL_PORT
 export TOR_SOCKS_PORT
 export TOR_CONTROL_PASSWD
 export UID_PORT
 export PORT_FILESERVER
 
-mv /app/ZeroNet/plugins/disabled-UiPassword /app/ZeroNet/plugins/UiPassword
 # Setup ZeroNet config if not exists
 ZERONET_CONFIG="/app/config/zeronet.conf"
 if [ ! -f "$ZERONET_CONFIG" ]; then
@@ -47,7 +43,6 @@ tor = $ENABLE_TOR
 tor_controller = tor:$TOR_CONTROL_PORT
 tor_proxy = tor:$TOR_SOCKS_PORT
 tor_password = $TOR_CONTROL_PASSWD
-ui_password = $UI_PASSWORD
 EOF
 fi
 
