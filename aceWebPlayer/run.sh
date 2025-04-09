fbb267a9cc52da8e4abdecb9a7f82193eb2cbae7
asopaipa <eslounicoquese@gmail.com>
2025-04-10 01:48:58 +0200

    Update run.sh

--- aceWebPlayer/run.sh
+++ aceWebPlayer/run.sh
@@ -6,11 +6,13 @@ if [ -f "$CONFIG_FILE" ]; then
     UI_USERNAME=$(jq -r '.UI_USERNAME // "user"' "$CONFIG_FILE")
     UI_PASSWORD=$(jq -r '.UI_PASSWORD // "user"' "$CONFIG_FILE")
     ACESTREAM_PORT=$(jq -r '.ACESTREAM_PORT // "6878"' "$CONFIG_FILE")
+    SLUG_ZERONET=$(jq -r '.SLUG_ZERONET // ""' "$CONFIG_FILE")
 else
     echo "No se encontró $CONFIG_FILE, usando valores por defecto."
     UI_USERNAME="user"
     UI_PASSWORD="user"
     ACESTREAM_PORT="6878"
+    SLUG_ZERONET=""
 fi
 
 
@@ -26,6 +28,12 @@ fi
 if [ -n "$UI_USERNAME" ]; then
     sed -i "s/USERNAME = \"\"/USERNAME = \"$UI_USERNAME\"/g" /app.py
     sed -i "s/PASSWORD = \"\"/PASSWORD = \"$UI_PASSWORD\"/g" /app.py
+fi
+
+if [ -n "$SLUG_ZERONET" ]; then
+    sed -i "s/config_zeronet_ws_url/\"ws://$SLUG_ZERONET:43110/Websocket\"/g" /app.py
+else
+    sed -i "s/config_zeronet_ws_url/\"ws://127.0.0.1:43110/Websocket\"/g" /app.py
 fi
 
 #cd /app
