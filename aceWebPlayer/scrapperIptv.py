bd1d7b248bcca972d05a97e1c0b32fabc2555765
asopaipa <eslounicoquese@gmail.com>
2025-04-16 17:56:19 +0200

    Update scrapperIptv.py

--- aceWebPlayer/scrapperIptv.py
+++ aceWebPlayer/scrapperIptv.py
@@ -10,6 +10,7 @@ from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Optional
 import logging
 import asyncio
+from playwright.async_api import async_playwright
 
 # Configuración de logging
 logging.basicConfig(
@@ -309,7 +310,7 @@ class ScraperManager:
         filtered_rows = []
         if all_rows:   
             for row in all_rows:
-                found_streams = asyncio.run(scan_streams(row.get("channel_url", "")))
+                found_streams = asyncio.run(self.scan_streams(row.get("channel_url", "")))
                 if found_streams and found_streams[0] and found_streams[0]["url"] and found_streams[0]["headers"]:
                     row["url_stream"] = found_streams[0]["url"]
                     row["headers"] = found_streams[0]["headers"]
