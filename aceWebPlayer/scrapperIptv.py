46e3c17f92937f8dbce85007035cd6c3a941ff93
asopaipa <eslounicoquese@gmail.com>
2025-04-16 18:01:46 +0200

    Update scrapperIptv.py

--- aceWebPlayer/scrapperIptv.py
+++ aceWebPlayer/scrapperIptv.py
@@ -308,7 +308,7 @@ class ScraperManager:
         filtered_rows = []
         if all_rows:   
             for row in all_rows:
-                found_streams = asyncio.run(scan_streams(row.get("channel_url", "")))
+                found_streams = asyncio.run(self.scan_streams(row.get("channel_url", "")))
                 if found_streams and found_streams[0] and found_streams[0]["url"] and found_streams[0]["headers"]:
                     row["url_stream"] = found_streams[0]["url"]
                     row["headers"] = found_streams[0]["headers"]
